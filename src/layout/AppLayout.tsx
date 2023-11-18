63b36dc16d2b264b400d715f96b0d3a3589449c6
Julio Benavente <julio.benavente.02@gmail.com>
2023-11-18 20:08:17 +0000

    Logo and User card added to sidebar

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,9 +1,17 @@
+import cn from "@/helpers/cn";
 import Sidebar from "./Sidebar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="bg-yellow-50 min-h-screen grid grid-cols-1 grid-rows-[1fr_auto]">
-      <main className="max-h-[calc(100vh-4rem)] overflow-y-auto">
+    <section
+      className={cn(
+        "bg-yellow-50 min-h-screen grid grid-cols-1 grid-rows-[1fr_auto]",
+        "lg:grid-cols-[300px_1fr] lg:grid-rows-none"
+      )}
+    >
+      <main
+        className={cn("max-h-[calc(100vh-4rem)] overflow-y-auto", "lg:order-2")}
+      >
         {children}
       </main>
       <Sidebar />
--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -2,27 +2,58 @@ import { SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import MoreNavItems from "./MoreNavItems";
 import navbarLinks from "./navbarLinks";
+import cn from "@/helpers/cn";
+import Link from "next/link";
+import { Typography } from "@/components";
+import MoreHoriz from "@mui/icons-material/MoreHoriz";
+import UserCard from "./UserCard";
 
 const Sidebar = () => {
   return (
-    <section className="">
-      <div className="">
-        <div className="">
-          <nav>
-            <ul className="grid grid-cols-5 h-16 border-t border-black relative">
-              {navbarLinks.slice(0, 4).map((e, i) => {
-                return <NavbarItem key={i} {...e} />;
-              })}
-              <MoreNavItems navItems={navbarLinks.slice(4)} />
-            </ul>
-          </nav>
-        </div>
-      </div>
+    <section className="grid bg-black grid-rows-[auto_1fr_auto] h-screen">
+      <Logo />
+      <nav>
+        <SmallScreenNavbarLink />
+        <BigScreenNavbarLink />
+      </nav>
+      <UserCard />
     </section>
   );
 };
 
 export default Sidebar;
+
+const Logo = () => {
+  return (
+    <div className="text-white mt-6 mb-8 mx-8 text-2xl font-bold">Zaafoo</div>
+  );
+};
+
+const SmallScreenNavbarLink = () => {
+  return (
+    <ul className="grid grid-cols-5 h-16 border-t border-black relative lg:hidden">
+      {navbarLinks.slice(0, 4).map((e, i) => {
+        return <NavbarItem key={i} {...e} />;
+      })}
+      <MoreNavItems navItems={navbarLinks.slice(4)} />
+    </ul>
+  );
+};
+
+const BigScreenNavbarLink = () => {
+  return (
+    <ul
+      className={cn(
+        "hidden",
+        "lg:grid grid-cols-1 h-full border-t border-black relative py-4 px-4 gap-y-2  content-start"
+      )}
+    >
+      {navbarLinks.map((e, i) => {
+        return <NavbarItem key={i} {...e} />;
+      })}
+    </ul>
+  );
+};
 
 export interface NavbarItemProps {
   path: string;
@@ -33,11 +64,18 @@ export interface NavbarItemProps {
 
 export const NavbarItem = (props: NavbarItemProps) => {
   return (
-    <div
-      className="group w-full h-full grid place-items-center"
+    <Link
+      href={props.path}
+      className={cn(
+        "group w-full h-full grid place-items-center",
+        "lg:grid-cols-[auto_1fr] lg:items-center lg:justify-start lg:place-items-stretch lg:gap-x-2 lg:py-3 lg:px-5 lg:rounded-sm text-white hover:bg-white/10 lg:h-max"
+      )}
       onClick={props.onClick}
     >
-      <props.icon className="group-hover:text-purple-700 lg:transition-all" />
-    </div>
+      <props.icon className="group-hover:text-purple-500 lg:transition-all text-lg" />
+      <p className="hidden lg:block text-sm group-hover:text-purple-500 transition-all">
+        {props.label}
+      </p>
+    </Link>
   );
 };
